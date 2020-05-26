acb65f4c19184b25a39edf550c0eadf0293734db
william <william@williamde-MacBook-Pro-2.local>
2020-05-26 15:41:15 +0800

    add LIME

--- explainers.py
+++ explainers.py
@@ -1,7 +1,7 @@
 import copy
 import numpy as np
 from tqdm import tqdm
-from sklearn.linear_model import LassoLars
+from sklearn.linear_model import Ridge, LassoLars
 
 import torch
 from torch_geometric.nn import MessagePassing
@@ -17,6 +17,8 @@ class GraphLIME:
         self.rho = rho
         self.cached = cached
         self.cached_result = None
+
+        self.model.eval()
 
     def __flow__(self):
         for module in self.model.modules():
@@ -42,6 +44,23 @@ class GraphLIME:
             kwargs[key] = item
 
         return x, y, edge_index, mapping, edge_mask, kwargs
+
+    def __init_predict__(self, x, edge_index, **kwargs):
+        if self.cached and self.cached_result is not None:
+            if x.size(0) != self.cached_result.size(0):
+                raise RuntimeError(
+                    'Cached {} number of nodes, but found {}.'.format(
+                        x.size(0), self.cached_result.size(0)))
+
+        if not self.cached or self.cached_result is None:
+            # Get the initial prediction.
+            with torch.no_grad():
+                log_logits = self.model(x=x, edge_index=edge_index, **kwargs)
+                probas = log_logits.exp()
+
+            self.cached_result = probas
+
+        return self.cached_result
     
     def __compute_kernel__(self, x, reduce):
         assert x.ndim == 2, x.shape
@@ -75,23 +94,7 @@ class GraphLIME:
         return G
         
     def explain_node(self, node_idx, x, edge_index, **kwargs):
-        if self.cached and self.cached_result is not None:
-            if x.size(0) != self.cached_result.size(0):
-                raise RuntimeError(
-                    'Cached {} number of nodes, but found {}.'.format(
-                        x.size(0), self.cached_result.size(0)))
-
-        if not self.cached or self.cached_result is None:
-            self.model.eval()
-
-            # Get the initial prediction.
-            with torch.no_grad():
-                log_logits = self.model(x=x, edge_index=edge_index, **kwargs)
-                probas = log_logits.exp()
-
-            self.cached_result = probas
-
-        probas = self.cached_result
+        probas = self.__init_predict__(x, edge_index, **kwargs)
 
         x, probas, _, _, _, _ = self.__subgraph__(
             node_idx, x, probas, edge_index, **kwargs)
@@ -116,14 +119,17 @@ class GraphLIME:
         return solver.coef_
 
 
-class Greedy:
+class LIME:
 
-    def __init__(self, model, cached=True):
+    def __init__(self, model, num_samples, cached=True):
         self.model = model
+        self.num_samples = num_samples
         self.cached = cached
         self.cached_result = None
 
-    def explain_node(self, node_idices, x, edge_index, **kwargs):
+        self.model.eval()
+
+    def __init_predict__(self, x, edge_index, **kwargs):
         if self.cached and self.cached_result is not None:
             if x.size(0) != self.cached_result.size(0):
                 raise RuntimeError(
@@ -131,8 +137,6 @@ class Greedy:
                         x.size(0), self.cached_result.size(0)))
 
         if not self.cached or self.cached_result is None:
-            self.model.eval()
-
             # Get the initial prediction.
             with torch.no_grad():
                 log_logits = self.model(x=x, edge_index=edge_index, **kwargs)
@@ -140,11 +144,74 @@ class Greedy:
 
             self.cached_result = probas
 
+        return self.cached_result
+
+    def explain_node(self, node_idx, x, edge_index, **kwargs):
+        probas = self.__init_predict__(x, edge_index, **kwargs)
+        proba, label = probas[node_idx, :].max(dim=0)
+        
+        x_ = copy.deepcopy(x)
+        original_feats = x[node_idx, :]
+
+        sample_x = [original_feats.detach().numpy()]
+        sample_y = [proba.item()]
+        
+        for _ in tqdm(range(self.num_samples), desc='collect samples', leave=False):
+            x_[node_idx, :] = original_feats + torch.randn_like(original_feats)
+            
+            with torch.no_grad():
+                log_logits = self.model(x=x_, edge_index=edge_index, **kwargs)
+                probas_ = log_logits.exp()
+
+            proba_ = probas_[node_idx, label]
+
+            sample_x.append(x_[node_idx, :].detach().numpy())
+            sample_y.append(proba_.item())
+
+        sample_x = np.array(sample_x)
+        sample_y = np.array(sample_y)
+
+        solver = Ridge(alpha=0.1)
+        solver.fit(sample_x, sample_y)
+
+        return solver.coef_
+
+
+class Greedy:
+
+    def __init__(self, model, cached=True):
+        self.model = model
+        self.cached = cached
+        self.cached_result = None
+
+        self.model.eval()
+
+    def __init_predict__(self, x, edge_index, **kwargs):
+        if self.cached and self.cached_result is not None:
+            if x.size(0) != self.cached_result.size(0):
+                raise RuntimeError(
+                    'Cached {} number of nodes, but found {}.'.format(
+                        x.size(0), self.cached_result.size(0)))
+
+        if not self.cached or self.cached_result is None:
+            # Get the initial prediction.
+            with torch.no_grad():
+                log_logits = self.model(x=x, edge_index=edge_index, **kwargs)
+                probas = log_logits.exp()
+
+            self.cached_result = probas
+
+        return self.cached_result
+
+    def explain_node(self, node_idices, x, edge_index, **kwargs):
+        if isinstance(node_idices, int):
+            node_idices = [node_idices]
+
+        probas = self.__init_predict__(x, edge_index, **kwargs)
+        probas, labels = probas[node_idices, :].max(dim=1)  # (m,), (m,)
+
         num_nodes, num_feats = len(node_idices), x.size(1)
         delta_probas = np.zeros((num_nodes, num_feats))  # (m, #feats)
-
-        probas = self.cached_result
-        probas, labels = probas[node_idices, :].max(dim=1)  # (m,), (m,)
 
         self.model.eval()
 
