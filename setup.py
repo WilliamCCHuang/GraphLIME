ff27bf1ff2f22f97a1d75b8c11d1a9518768068b
william <william@williamde-MacBook-Pro-2.local>
2020-11-26 16:35:50 +0800

    commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="graphlime",
-    version="0.1.1",
+    version="0.1.2",
     author="williamcchuang",
     author_email="hang6318179@gmail.com",
     description="A package",
